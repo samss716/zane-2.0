5ff34c6aeb4faab60ee59be65c21b8cbfd25e50f
samss716 <samanthasanchez887@gmail.com>
2025-09-17 18:51:23 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-white">
+      <h1 className="text-4xl font-bold tracking-tight">Zane Website</h1>
+    </main>
+  );
+}
