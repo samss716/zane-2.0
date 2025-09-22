d26d8a414c63d0e833d43c3c4e31a3acfd97bc3b
samss716 <your-username@users.noreply.github.com>
2025-09-22 16:04:12 -0400

    adjusting layout

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,32 +2,32 @@ export default function HomePage() {
   return (
     <main className="min-h-screen bg-[#fffbec] relative">
       {/* Hero Section */}
-      <section className="relative h-[60vh] bg-gradient-to-r from-purple-500 to-indigo-600 flex items-center justify-center overflow-hidden">
+      <section className="relative border h-[40vh] flex flex-col items-center justify-center overflow-hidden">
         
 
         {/* Text will go here */}
-        <h1 className="text-white text-6xl font-bricolage">
+        <h1 className="text-white = text-6xl font-bricolage">
           Portfolio
         </h1>
       </section>
 
       {/* Rest of page below */}
-      <section className="h-[40vh] flex items-center justify-center">
+      <section className="h-[40vh] flex flex-col items-center justify-center">
         <p className="text-xl font-bricolage">Other content goes here…</p>
       </section>
 
        {/* About Section */}
-      <section className="h-screen flex items-center justify-center bg-white">
+      <section className="h-screen flex flex-col items-center justify-center bg-white">
         <h2 className="text-4xl font-bricolage">About Me</h2>
       </section>
 
       {/* Portfolio Section */}
-      <section className="h-screen flex items-center justify-center bg-[#f5f5f5]">
+      <section className="h-screen flex flex-col items-center justify-center bg-[#f5f5f5]">
         <h2 className="text-4xl font-bricolage">Portfolio Works</h2>
       </section>
 
       {/* Contact Section */}
-      <section className="h-[50vh] flex items-center justify-center bg-[#0099ff] text-white">
+      <section className="h-[50vh] flex flex-col items-center justify-center bg-[#0099ff] text-white">
         <h2 className="text-4xl font-bricolage">Contact</h2>
       </section>
 
